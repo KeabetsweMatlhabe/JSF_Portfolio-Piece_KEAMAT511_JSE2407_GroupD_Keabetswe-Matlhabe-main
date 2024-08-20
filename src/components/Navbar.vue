f58ce041307701967c13ef349368d0dfcc4052b9
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-08-20 10:27:24 +0200

    added dark: variants to the background and text colors

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,32 +1,32 @@
 <template>
-  <nav class="navbar bg-gray-800 p-4">
+  <nav class="navbar bg-gray-800 dark:bg-gray-900 p-4">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
       <router-link to="/">
         <button class="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="../assets/online-shop.png" class="h-8" alt="SwiftCart Logo" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">SwiftCart</span>
+          <span class="self-center text-2xl font-semibold whitespace-nowrap text-white dark:text-gray-200">SwiftCart</span>
         </button>
       </router-link>
       <ul class="flex space-x-8">
         <li>
-          <router-link to="/wishlist" class="text-white hover:text-blue-500">Wishlist</router-link>
+          <router-link to="/wishlist" class="text-white dark:text-gray-200 hover:text-blue-500">Wishlist</router-link>
         </li>
         <li>
-          <router-link to="/cart" class="text-white hover:text-blue-500">
+          <router-link to="/cart" class="text-white dark:text-gray-200 hover:text-blue-500">
             Cart ({{ cartStore.cartCount }})
           </router-link>
         </li>
         <li v-if="cartStore.isLoggedIn">
-          <button @click="handleLogout" class="text-white hover:text-blue-500">Logout</button>
+          <button @click="handleLogout" class="text-white dark:text-gray-200 hover:text-blue-500">Logout</button>
         </li>
         <li v-else>
-          <router-link to="/login" class="text-white hover:text-blue-500">Login</router-link>
+          <router-link to="/login" class="text-white dark:text-gray-200 hover:text-blue-500">Login</router-link>
         </li>
         <li v-if="cartStore.isLoggedIn">
-      <router-link to="/comparison" class="text-white hover:text-blue-500">
-        Compare ({{ comparisonStore.comparisonCount }})
-      </router-link>
-    </li>
+          <router-link to="/comparison" class="text-white dark:text-gray-200 hover:text-blue-500">
+            Compare ({{ comparisonStore.comparisonCount }})
+          </router-link>
+        </li>
       </ul>
     </div>
   </nav>
